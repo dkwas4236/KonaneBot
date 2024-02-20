bf6665b9fe5c3c0da72bbb3b5a1a9adcb8d55cf0
John Divinagracia <divinagraciaj@students.cs.macewan.ca>
2024-02-20 01:39:36 -0700

    Modified bitboard calculations: bit shifts used instead

--- src/boardio.c
+++ src/boardio.c
@@ -2,18 +2,6 @@
 #include <stdio.h>
 #include "boardio.h"
 
-// function will help get bitboard of input file
-uint64_t pow(int x, int y)
-{
-  uint64_t result = 1;
-  int power = y;
-  if (power == 0) return 1;
-  while (power > 0) {
-    result *= x;
-    power--;
-  }
-}
-
 // get bitboard of input file. make sure index
 // is only either 0 (black pieces) or 1 (white pieces). 
 uint64_t* getBitBoard(char* fileName) {
@@ -22,14 +10,16 @@ uint64_t* getBitBoard(char* fileName) {
 
   FILE* readFile = fopen(fileName, "r");
   uint64_t b = 0, w = 0;
-  int currBit = 0;
 
   char c;
   while ((c = fgetc(readFile)) != EOF) {
     if (c == '\n') continue;
-    else if (c == 'B') b += pow(2, currBit);
-    else if (c == 'W') w += pow(2, currBit);
-    currBit++;
+
+    b <<= 1;
+    w <<= 1;
+
+    if (c == 'B') b++;
+    else if (c == 'W') w++;
   }
 
   fclose(readFile);
